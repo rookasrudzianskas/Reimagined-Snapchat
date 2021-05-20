72b05d93ee9cecd16ea5d04086c0e32e41fc2da0
Rokas <rookas.rudzenskas@gmail.com>
2021-05-20 17:38:01 +0300

    Roaks

---
+++ src/features/appSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState: {
+    user: null,
+    selectedImage: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+
+    logout: (state) => {
+      state.user = null
+    },
+    selectImage: (state, action) => {
+      state.selectedImage = action.payload;
+    },
+
+    resetImage: (state, action) => {
+      state.selectedImage = null;
+    },
+  },
+});
+
+export const { login, logout, selectImage, resetImage } = appSlice.actions;
+
+export const selectSelectedImage = state => state.app.selectedImage;
+export const selectUser = state => state.app.user;
+
+
+export default appSlice.reducer;
