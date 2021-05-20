7507b7a7d2d2c2b09edf051f68cefd6aa7b64fd2
Rokas <rookas.rudzenskas@gmail.com>
2021-05-20 17:43:54 +0300

    Rokas

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAFQeZ3qJVudx4slmLzVBqMJkIGMgcY15k",
+    authDomain: "rookas-snapchat-app.firebaseapp.com",
+    projectId: "rookas-snapchat-app",
+    storageBucket: "rookas-snapchat-app.appspot.com",
+    messagingSenderId: "756891412147",
+    appId: "1:756891412147:web:337cc688667bef218af2c6",
+    measurementId: "G-6L4SGY8MGW"
+};
+
