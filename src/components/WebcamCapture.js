8e7ccac614b4c67c9caa7b3943496c9ab7bb5a38
Rokas <rookas.rudzenskas@gmail.com>
2021-05-20 17:55:39 +0300

    Rokas

---
+++ src/components/WebcamCapture.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const videoConstrains = {
+    width: 250,
+    height: 400,
+    facingMode: "user",
+};
+
+const WebcamCapture = () => {
+    return (
+        <div className="webcamCapture">
+
+        </div>
+    );
+};
+
+export default WebcamCapture;
