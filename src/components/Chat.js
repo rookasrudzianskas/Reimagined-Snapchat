8eae713a587f0938cbc1a675cbfc51a6c6259471
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 08:26:50 +0300

    Roaks

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,14 +4,29 @@ import {Avatar} from "@material-ui/core";
 import {StopRounded} from "@material-ui/icons";
 import ReactTimeago from  "react-timeago"
 import {useDispatch} from "react-redux";
+import {selectImage} from "../features/appSlice";
+import db from "../firebase";
+import {useHistory} from "react-router-dom";
+
 const Chat = ({profilePic, id, username, imageUrl, timestamp, read, key}) => {
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const open = () => {
         if(!read) {
-            dispatch(selectImage)
+            // then I click on the image, it will go ahead, and push the image to the store, for us to be able to render it to the as the preview
+            dispatch(selectImage(imageUrl));
+            db.collection('posts').doc(id).set({
+                read: true,
+                // it updates the firebase post, but not overwrites everything, but keeps everything with merge and adds the read true as replaced
+                // read false value
+                // merge means to keep the previous state too
+            }, {merge: true}
+            );
+            // push to the view component
+            history.push('/chats/view');
         }
-    }
+    };
 
 
     return (
