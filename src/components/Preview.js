477dba51a810e7c28dc646f21a7448f83c3e5c31
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 06:52:35 +0300

    Rokas

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/Preview.css";
+import {useDispatch, useSelector} from "react-redux";
+import {selectCameraImage} from "../features/cameraSlice";
 
 const Preview = () => {
+
+    const dispatch = useDispatch()
+    // will pull camera image from data layer
+    const cameraImage = useSelector(selectCameraImage)
     return (
         <div className="preview">
-
+            <img src={cameraImage} alt=""/>
         </div>
     );
 };
--- src/components/WebcamCapture.js
+++ src/components/WebcamCapture.js
@@ -3,7 +3,8 @@ import Webcam from "react-webcam";
 import RadioButtonUnchecked from '@material-ui/icons/RadioButtonUnchecked';
 import {useDispatch} from "react-redux";
 import {setCameraImage} from "../features/cameraSlice";
-
+import { useHistory } from "react-router-dom";
+import "./styles/WecamCapture.css"
 // video camera settings
 const videoConstrains = {
     width: 250,
@@ -16,7 +17,7 @@ const WebcamCapture = () => {
     const webcamRef = useRef(null);
     const dispatch = useDispatch();
     const [image, setImage] = useState(null);
-
+    const history = useHistory()
     // use callback is clever function it does the same, then is fired for the first time, and after that, then fired next times, it just
     // uses the same answer as from the first rerender
     const capture = useCallback(() => {
@@ -25,7 +26,7 @@ const WebcamCapture = () => {
         const imageSrc = webcamRef.current.getScreenshot();
         // dispatching to the data layer
         dispatch(setCameraImage(imageSrc));
-
+        history.push("./preview")
         // console.log(imageSrc) base 64 image
         setImage(imageSrc);
 
