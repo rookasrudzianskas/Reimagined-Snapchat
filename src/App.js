813211bc53d3902ed033c99d579cfdf4cd6713bb
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 06:38:50 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from "./components/WebcamCapture";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello 🚀</h1>
-        <WebcamCapture />
+        <Router>
+            <div className="app__body">
+                <Switch>
+                    <Route path="/test">
+                        <h1>This is test</h1>
+                    </Route>
+                    <Route exact path="/">
+                        <WebcamCapture />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
