de0b8f374bace6f20edc8b81fc92b6aa5b2602d1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 07:55:15 +0300

    Roaks

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Chats.css";
 import {Avatar} from "@material-ui/core";
 import { ChatBubble, Search } from '@material-ui/icons';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import db from "../firebase";
 
 const Chats = () => {
+
+    const [posts, setPosts] = useState();
+    console.log(posts);
+    useEffect(() => {
+        // then db changes, it automatically updates the post
+        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data(),
+            })))
+        });
+    }, []);
     return (
         <div className="chats">
             <div className="chats__header">
@@ -18,7 +31,7 @@ const Chats = () => {
             </div>
 
             <div className="chats__posts">
-
+                <h2>Hello</h2>
             </div>
         </div>
     );
