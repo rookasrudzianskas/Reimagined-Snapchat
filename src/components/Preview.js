d5a23d4a0215192c4449fe42e5d8afccf25fdda0
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 06:55:04 +0300

    Roaks

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Preview.css";
 import {useDispatch, useSelector} from "react-redux";
 import {selectCameraImage} from "../features/cameraSlice";
-
+import { useHistory } from "react-router-dom"
 const Preview = () => {
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     // will pull camera image from data layer
-    const cameraImage = useSelector(selectCameraImage)
+    const cameraImage = useSelector(selectCameraImage);
+    const history = useHistory();
+
+    useEffect(() => {
+        if(!cameraImage) {
+            // will replace the page to camera page
+            history.replace("/");
+            // works, redirects if there is no image, dependent to the camera image and history, variables from outside
+        }
+    }, [cameraImage, history]);
     return (
         <div className="preview">
             <img src={cameraImage} alt=""/>
